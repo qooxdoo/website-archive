dc3c4f09e35803279182d7c9613dd14c00c62da2
Travis CI deploy <no-reply@qooxdoo.org>
2017-01-18 10:37:38 +0000

    Refresh site at 49af188

--- devel/feedreader-mobile/script/feedreader.js
+++ devel/feedreader-mobile/script/feedreader.js
@@ -5,7 +5,7 @@ if (!window.qx) window.qx = {};
 qx.$$start = new Date();
 
 if (!qx.$$environment) qx.$$environment = {};
-var envinfo = {"qx.application":"feedreader.MobileApplication","qx.debug":false,"qx.debug.databinding":false,"qx.debug.dispose":false,"qx.debug.io":false,"qx.debug.ui.queue":false,"qx.mobile.emulatetouch":true,"qx.nativeScrollBars":true,"qx.optimization.basecalls":true,"qx.optimization.comments":true,"qx.optimization.privates":true,"qx.optimization.strings":true,"qx.optimization.variables":true,"qx.optimization.variants":true,"qx.optimization.whitespace":true,"qx.revision":"HEAD:079c1a1","qx.theme":"qx.theme.Indigo","qx.version":"5.1-079c1a1"};
+var envinfo = {"qx.application":"feedreader.MobileApplication","qx.debug":false,"qx.debug.databinding":false,"qx.debug.dispose":false,"qx.debug.io":false,"qx.debug.ui.queue":false,"qx.mobile.emulatetouch":true,"qx.nativeScrollBars":true,"qx.optimization.basecalls":true,"qx.optimization.comments":true,"qx.optimization.privates":true,"qx.optimization.strings":true,"qx.optimization.variables":true,"qx.optimization.variants":true,"qx.optimization.whitespace":true,"qx.revision":"HEAD:49af188","qx.theme":"qx.theme.Indigo","qx.version":"5.1-49af188"};
 for (var k in envinfo) qx.$$environment[k] = envinfo[k];
 
 if (!qx.$$libraries) qx.$$libraries = {};
